f7a5ee954d0724a4f21e6cafeaa4e7521dfa0075
8ctambules <philippe.hubert@sciencespo.fr>
2016-09-22 11:42:56 +0200

    Added corporate and individual sections for people

--- data_specs/global_schema.js
+++ data_specs/global_schema.js
@@ -1,156 +1,193 @@
 var peopleSchema = new Schema({ 
-	isari_authorized_centers : [{ 
-		organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
-		isari_role : { 
-			type : String, 
-			enum : enums.isari_authorized_centers, 
-		 }
-	 }], 
-	firstname : String, 
-	name : { 
-		type : String,
-		required : true
-	}, 
-	gender : { 
-		type : String, 
-		enum : enums.genders
-	 }, 
-	birthdate : Date, 
-	nationalities : [{ 
-		type : String, 
-		enum : enums.countries.nationality, 
-	 }], 
-	ldap_uid : String, 
-	banner_uid : String, 
-	SIRH_matricule : String, 
-	ORCID : String, 
-	IDREF : String, 
-	SPIRE_ID : String,
-	positions : [{ 
+	corporate : {
+		isari_authorized_centers : [{ 
 			organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
-			start_date : Date, 
-			end_date : Date, 
-			timepart : { type : Number, default : 1 , min:0.05, max:1 },
-			job_name : String,
-			job_type : { 
+			isari_role : { 
 				type : String, 
-				enum : enums.job_type
-			 }, 						
-			job_title : { 
-				type : String, 
-				enum : enums.job_title
-			 }, 
-			grades : [{
-					grade :{
-						type : String, 
-						enum : enums.grade
-					}, 
-					start_date : Date, 
-					end_date : Date
-					// validation custom à coder , dates grades contenus dans les dates de positions
-			}], 
-			bonuses : [{ 
-				bonusType : { 
-					type : String, 
-					enum : enums.bonusTypes
-				 }, 
-				start_date : Date, 
-				end_date : Date
-				// validation custom à coder , dates grades contenus dans les dates de positions
-		 	}]
-	}],
-	academicMembership : [
-		{	
-			organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
-			start_date : Date, 
-			end_date : Date,
-			membership_type : {
-				type : String,
-				enum : enums.academicMembership
-			} 
-		}
-	], 
-	contacts : [{
-		title : String,
-		email : { type : String, match:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ }, 
-		phone : { type : String }
-	 }], 
-	//photo_filename : String, 
-	biography : String, 
-	tags : { 
-		hceres_2017 : [{ 
-			type : String, 
-			enum : enums.hceres_2017
-		 }],  
-		aeres_2012 : [{ 
-			type : String, 
-			enum : enums.aneres_2012
-		 }],  
-		methods : [{ 
-			type : String, 
-			enum : enums.methods
+				enum : enums.isari_authorized_centers, 
+			 }
 		 }], 
-		free : Array, 
-		erc : [{ 
-			type : String, 
-			enum : enums.erc
-		 }],  
-		discipline : [{ 
-			type : String, 
-			enum : enums.disciplines
-		 }], 
-		research_theme : [{ 
-			type : String, 
-			enum : enums.research_themes
-		 }],
-		 section_CNU : [{ 
-		 	type : String,
-		 	enum : enums.section_cnu
-		 }],
-		 section_CNRS : [{
-		 	type : String,
-		 	enum : enums.section_cnrs
-		 }]
-	 },
-	langs:[{
-		type : String,
-		enum : enums.iso6391
-	}],
-	personal_activities : [
-	 	{
-	 		personalActivityType : {
-	 			type : String,
-	 			enum : enums.personalActivityType,
-			    required : true
-		    },
-		    personalActivitySubtype : String,
-	 		personnalActivityTitle : {
-	 			type : String
-	 			// choix guidé par une liste de valuer qui dépends de personalActivityType
-	 		},
-	 		start_date : Date,
-	 		end_date : Date,
-	 		role : String,
-	 		description : String,
-	 		organizations : [{ type : Schema.Type.ObjectId, ref : 'Organization' }],
-			peoples : [{ type : Schema.Type.ObjectId, ref : 'People' }]
-	 	}
-	 	// eneignemenet et encadrement demandent plus de champs... on les sépare ?
-	], 
-	distinctions : [{
-		organizations : [{ type : Schema.Type.ObjectId, ref : 'Organization' }], 
-		date : Date, 
-		title : { 
+		firstname : String,
+		name : { 
 			type : String,
 			required : true
 		}, 
-		distinctionType : { 
+		gender : { 
 			type : String, 
-			enum : enums.distinctionTypes
+			enum : enums.genders
 		 }, 
-		subject : String, 
-		honours : String //désolé, honours est toujours au pluriel. Faut pas se laisser aller à la mauvaise aurtaugrafe non plus.
-	 }] 
+	 	birthdate : Date, 
+	 	nationalities : [{ 
+	 		type : String, 
+	 		enum : enums.countries.nationality, 
+	 	 }], 
+		ldap_uid : String, 
+		banner_uid : String, 
+		SIRH_matricule : String, 
+	 	SPIRE_ID : String,
+	 	positions : [{ 
+	 			organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
+	 			start_date : Date, 
+	 			end_date : Date, 
+	 			timepart : { type : Number, default : 1 , min:0.05, max:1 },
+	 			job_name : String,
+	 			job_type : { 
+	 				type : String, 
+	 				enum : enums.job_type
+	 			 }, 						
+	 			job_title : { 
+	 				type : String, 
+	 				enum : enums.job_title
+	 			 }, 
+	 			grades_admin : [{
+	 					grade : {
+	 						type : String, 
+	 						enum : enums.grade
+	 					}, 
+	 					start_date : Date, 
+	 					end_date : Date
+	 					// validation custom à coder , dates grades contenus dans les dates de positions
+	 			}], 
+	 			grades_academic : [{
+	 					grade : {
+	 						type : String, 
+	 						enum : enums.grade
+	 					}, 
+	 					start_date : Date, 
+	 					end_date : Date
+	 					// validation custom à coder , dates grades contenus dans les dates de positions
+	 			}], 
+	 			bonuses : [{ 
+	 				bonusType : { 
+	 					type : String, 
+	 					enum : enums.bonusTypes
+	 				 }, 
+	 				start_date : Date, 
+	 				end_date : Date
+	 				// validation custom à coder , dates grades contenus dans les dates de positions
+	 		 	}]
+	 	}],
+		academicMembership : [
+			{	
+				organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
+				start_date : Date, 
+				end_date : Date,
+				membership_type : {
+					type : String,
+					enum : enums.academicMembership
+				} 
+			}
+		], 
+	},
+	individual : {
+		ORCID : String, 
+		IDREF : String, 
+		former_positions : [{ 
+				organization : { type : Schema.Type.ObjectId, ref : 'Organization' }, 
+				start_date : Date, 
+				end_date : Date, 
+				timepart : { type : Number, default : 1 , min:0.05, max:1 },
+				job_name : String,
+				job_type : { 
+					type : String, 
+					enum : enums.job_type
+				 }, 						
+				job_title : { 
+					type : String, 
+					enum : enums.job_title
+				 }, 
+				grades_academic : [{
+						grade : {
+							type : String, 
+							enum : enums.grade
+						}, 
+						start_date : Date, 
+						end_date : Date
+						// validation custom à coder , dates grades contenus dans les dates de positions
+				}], 
+		}],
+		biography : String, 
+		tags : { 
+			hceres_2017 : [{ 
+				type : String, 
+				enum : enums.hceres_2017
+			 }],  
+			aeres_2012 : [{ 
+				type : String, 
+				enum : enums.aneres_2012
+			 }],  
+			methods : [{ 
+				type : String, 
+				enum : enums.methods
+			 }], 
+			free : Array, 
+			erc : [{ 
+				type : String, 
+				enum : enums.erc
+			 }],  
+			discipline : [{ 
+				type : String, 
+				enum : enums.disciplines
+			 }], 
+			research_theme : [{ 
+				type : String, 
+				enum : enums.research_themes
+			 }],
+			 section_CNU : [{ 
+			 	type : String,
+			 	enum : enums.section_cnu
+			 }],
+			 section_CNRS : [{
+			 	type : String,
+			 	enum : enums.section_cnrs
+			 }]
+		 },
+	 	personal_activities : [
+	 	 	{
+	 	 		personalActivityType : {
+	 	 			type : String,
+	 	 			enum : enums.personalActivityType,
+	 			    required : true
+	 		    },
+	 		    personalActivitySubtype : String,
+	 	 		personnalActivityTitle : {
+	 	 			type : String
+	 	 			// choix guidé par une liste de valuer qui dépends de personalActivityType
+	 	 		},
+	 	 		start_date : Date,
+	 	 		end_date : Date,
+	 	 		role : String,
+	 	 		description : String,
+	 	 		organizations : [{ type : Schema.Type.ObjectId, ref : 'Organization' }],
+	 			peoples : [{ type : Schema.Type.ObjectId, ref : 'People' }]
+	 	 	}
+	 	 	// eneignemenet et encadrement demandent plus de champs... on les sépare ?
+	 	], 
+		distinctions : [{
+			organizations : [{ type : Schema.Type.ObjectId, ref : 'Organization' }], 
+			date : Date, 
+			title : { 
+				type : String,
+				required : true
+			}, 
+			distinctionType : { 
+				type : String, 
+				enum : enums.distinctionTypes
+			 }, 
+			subject : String, 
+			honours : String //désolé, honours est toujours au pluriel. Faut pas se laisser aller à la mauvaise aurtaugrafe non plus.
+		 }] 
+	 	contacts : [{
+	 		title : String,
+	 		email : { type : String, match:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ }, 
+	 		phone : { type : String }
+	 	 }], 
+	 	langs: [{
+	 		type : String,
+	 		enum : enums.iso6391
+	 	}],
+	//photo_filename : String, 
+	}
 });
 
 
