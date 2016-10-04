2830f9d484526a54fd59c16de3fa2f066157fe36
Nicolas Chambrier <naholyr@gmail.com>
2016-10-04 16:01:27 +0200

    feat(app): Implement /schemas/:name (refs #13)

--- server/routes/schemas.js
+++ server/routes/schemas.js
@@ -4,6 +4,7 @@ const { Router } = require('express')
 const { ClientError } = require('../lib/errors')
 const metas = require('../../specs/schema.meta.json')
 const { restHandler } = require('../lib/rest-utils')
+const { RESERVED_FIELDS } = require('../lib/schemas')
 
 module.exports = Router()
 .get('/:name', restHandler(getSchema))
@@ -14,14 +15,76 @@ function getSchema (req) {
 
 	for (let key in metas) {
 		if (key.toLowerCase() === name) {
-			return formatMeta(metas[key])
+			return formatMeta(metas[key], {
+				admin: false // TODO from web session
+			})
 		}
 	}
 
 	throw new ClientError({ title: `Unknown model "${req.params.name}"`, status: 404 })
 }
 
-function formatMeta (data) {
-	// TODO format for frontend needs
-	return Promise.resolve(data)
+function formatMeta (meta, options) {
+	return Promise.resolve(_formatMeta(meta, options))
+}
+
+const KEPT_FIELDS = [
+	'label',
+	'requirement',
+	'suggestions',
+	'type',
+	'ref',
+	'enum',
+	'default',
+	'min',
+	'max',
+	'regex'
+]
+
+function _formatMeta (meta, options = {}) {
+	const { admin = false } = options
+
+	const multiple = Array.isArray(meta)
+	const desc = multiple ? meta[0] : meta
+
+	const handleField = (result, name) => {
+		if (!result) {
+			return result // Skipped field
+		}
+
+		if (!RESERVED_FIELDS.includes(name)) {
+			// Sub-field: just include it
+			const subres = _formatMeta(desc[name], options)
+			if (subres) {
+				result[name] = subres
+			}
+		}
+
+		// Access type defines if we can see this field
+		else if (name === 'accessType' && !admin) {
+			result = null // Skip the field
+		}
+
+		// Field kept as-is
+		else if (KEPT_FIELDS.includes(name)) {
+			result[name] = desc[name]
+		}
+
+		// Other reserved fields are skipped
+
+		return result
+	}
+
+	let result = Object.keys(desc).reduce(handleField, {})
+	if (!result) {
+		// Skip this field
+		return result
+	}
+
+	// Handle multi-valued fields
+	if (multiple) {
+		result.multiple = true
+	}
+
+	return result
 }
