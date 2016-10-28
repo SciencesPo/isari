4f8fc26b9794f73899fa73a1b7f08fb1c948839e
Nicolas Chambrier <naholyr@gmail.com>
2016-10-28 15:46:16 +0200

    fix(ldap): fixes test-ldap script never connecting to mongo

--- server/test-ldap.js
+++ server/test-ldap.js
@@ -2,11 +2,13 @@
 
 /* eslint no-console:0 */
 
+const { connect } = require('./lib/model')
 const auth = require('./lib/auth')
 
 const login = process.argv[2]
 const password = process.argv[3]
 
-auth(login, password)
+connect()
+.then(() => auth(login, password))
 .then(() => (console.log('OK'), process.exit(0)))
 .catch(e => (console.error(e.message), process.exit(1)))
