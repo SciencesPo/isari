963fa52fbc5bc76d926b1180eeba8575be19f70c
t8g <tmoyse@gmail.com>
2016-11-14 16:02:08 +0100

    Add boolean field as checkbox Fix #81

---
+++ client/src/app/fields/isari-checkbox/isari-checkbox.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, EventEmitter, Output, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'isari-checkbox',
+  templateUrl: 'isari-checkbox.component.html'
+})
+export class IsariCheckboxComponent implements OnInit {
+
+  @Input() name: string;
+  @Input() form: FormGroup;
+  @Input() label: string;
+  @Input() description: string;
+  @Output() onUpdate = new EventEmitter<any>();
+
+  constructor() { }
+
+  update($event) {
+    if (this.onUpdate) {
+      this.onUpdate.emit($event);
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
