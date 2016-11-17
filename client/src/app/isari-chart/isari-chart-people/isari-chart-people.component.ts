dc8a82bafdcb52d5e901f3cc8e9eb2c65b3ecaa0
Yomguithereal <guillaumeplique@gmail.com>
2016-11-17 17:55:12 +0100

    Adding isari-chart-people

---
+++ client/src/app/isari-chart/isari-chart-people/isari-chart-people.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'isari-chart-people',
+  templateUrl: './isari-chart-people.component.html',
+  styleUrls: ['./isari-chart-people.component.css']
+})
+export class IsariChartPeopleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
