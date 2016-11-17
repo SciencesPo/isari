5f68c4c74fa9817ac2f157bbdc4d8ef908d61c9f
t8g <tmoyse@gmail.com>
2016-11-17 14:10:08 +0100

    redirect on unauthorized organization

--- client/src/app/organization.resolver.ts
+++ client/src/app/organization.resolver.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { UserService } from './user.service';
 
 @Injectable()
 export class OrganizationResolver implements Resolve<any> {
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot) {
-    return this.userService.getOrganization(route.queryParams['organization']);
+    return this.userService.getOrganization(route.queryParams['organization'])
+      .map(organization => {
+        if (!organization) {
+          this.router.navigate(['/']);
+        }
+        return organization;
+      });
   }
 
 }
