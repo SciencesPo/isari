9f3eb873757e3cba32028dcb83b19363e146b786
Yomguithereal <guillaumeplique@gmail.com>
2016-11-18 11:10:53 +0100

    Adding download button visuals

---
+++ client/src/app/isari-list/isari-download-button/isari-download-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'isari-download-button',
+  templateUrl: './isari-download-button.component.html',
+  styleUrls: ['./isari-download-button.component.css']
+})
+export class IsariDownloadButtonComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
