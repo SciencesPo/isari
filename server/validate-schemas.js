94e10a867a163ed2ff28f738f2e1c8c4ec59ed81
Nicolas Chambrier <naholyr@gmail.com>
2016-12-09 14:57:11 +0100

    fix(layouts): properly validates layouts on server start (case issue, refs #138)

--- server/validate-schemas.js
+++ server/validate-schemas.js
@@ -4,46 +4,51 @@ const config = require('config')
 const chalk = require('chalk')
 const { getFrontSchema, getMongooseSchema } = require('./lib/schemas')
 const { getLayout } = require('./lib/layouts')
+const { uniq } = require('lodash/fp')
+
+const { inspect } = require('util')
+const log = o => console.log(inspect(o, { colors: true, depth: 10 }))
+log(getLayout('People', true))
 
 
 console.log('Validating schemas and layouts…') // eslint-disable-line no-console
 
-let errors = []
+let invalids = []
 
 Object.keys(config.collections).forEach(name => {
 	try {
 		getMongooseSchema(name)
 	} catch (e) {
 		process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in schema (mongoose) "${name}"`)}:\n${chalk.red(e.message)}\n\n`)
-		errors.push(name)
+		invalids.push(name)
 	}
 	try {
 		getFrontSchema(name, false)
 	} catch (e) {
 		process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in schema (front, restricted excluded) "${name}"`)}:\n${chalk.red(e.message)}\n\n`)
-		errors.push(name)
+		invalids.push(name)
 	}
 	try {
 		getFrontSchema(name, true)
 	} catch (e) {
 		process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in schema (front, restricted included) "${name}"`)}:\n${chalk.red(e.message)}\n\n`)
-		errors.push(name)
+		invalids.push(name)
 	}
 	try {
 		getLayout(name, false)
 	} catch (e) {
 		process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in layout (restricted excluded) "${name}"`)}:\n${chalk.red(e.message)}\n\n`)
-		errors.push(name)
+		invalids.push(name)
 	}
 	try {
 		getLayout(name, true)
 	} catch (e) {
 		process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in layout (restricted included) "${name}"`)}:\n${chalk.red(e.message)}\n\n`)
-		errors.push(name)
+		invalids.push(name)
 	}
 })
-if (errors.length > 0) {
-	process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in following schemas: ${errors.join(', ')}`)}\n${chalk.red('Check errors above')}\n\n`)
+if (invalids.length > 0) {
+	process.stderr.write(`\n\n${chalk.bold.red(`Fatal error in following schemas: ${uniq(invalids).join(', ')}`)}\n${chalk.red('Check errors above')}\n\n`)
 	process.exit(1)
 }
 
