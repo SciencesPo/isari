995813307d8e1a054c7e11aa665b9a7a66e8d188
Nicolas Chambrier <naholyr@gmail.com>
2016-12-13 17:28:50 +0100

    feat(cv): sets current organization id in background (refs #139)

--- client/src/app/cv/cv.component.ts
+++ client/src/app/cv/cv.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'isari-cv',
@@ -10,14 +12,33 @@ export class CVComponent implements OnInit {
 
   private userId: string;
 
+  // Ensure we don't call data editor too soon: wait for organization to be resolved
+  private ready: boolean = false;
+
   constructor(
     private userService: UserService,
   ) { }
 
   ngOnInit () {
-    this.userService.isLoggedIn().subscribe(user => {
-      if (user && user.people) {
+    Observable.combineLatest(
+      this.userService.getOrganizations(),
+      this.userService.isLoggedIn()
+    ).subscribe((data: any) => { // TODO proper type definition
+      const [ { organizations, central }, user ] = data;
+      if (!user || !user.people) {
+        // TODO error message: invalid session status
+        this.ready = false;
+      } else if (!organizations || organizations.length === 0) {
+        // TODO error message: no organization accessible
+        this.ready = false;
+      } else if (organizations.length === 1 && central) {
+        // TODO error message: no organization accessible except the virtual global one
+        this.ready = false;
+      } else {
+        // User is logged in and has several organizations accessible: take the first one with a valid id
+        this.userService.setCurrentOrganizationId(organizations[0].id || organizations[1].id);
         this.userId = user.people.id;
+        this.ready = true;
       }
     });
   }
