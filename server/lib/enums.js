8ae934ea016a3d52d47ea9a181fa13a6c00574e7
Nicolas Chambrier <naholyr@gmail.com>
2016-12-14 21:46:32 +0100

    fix(enums): fixes lost countries, currencies, languages and nationalities in simple enum values (fixes #172)

--- server/lib/enums.js
+++ server/lib/enums.js
@@ -1,47 +1,103 @@
 'use strict'
 
 const enums = require('../../specs/enums.json')
-const nested = require('../../specs/enums.nested.json')
+const nestedEnums = require('../../specs/enums.nested.json')
+const countries = require('../../specs/enum.countries.json')
+const currencies = require('../../specs/enum.currencies.json')
+const languages = require('../../specs/enum.languages.json')
+const memoize = require('memoizee')
 
-exports.enumValueGetter = (zenum) => {
-	if (Array.isArray(zenum)) {
-		// As an array: direct values not exported into enums module
-		return simpleFind(zenum)
-	} else if (typeof zenum === 'string') {
-		if (zenum.indexOf(':') === -1) {
-			// Standard enum from enums.json
-			if (enums[zenum]) {
-				return simpleFind(enums[zenum])
-			} else {
-				throw Error(`Invalid enum "${zenum}", check your schema and/or enums.json`)
-			}
+
+exports.enumValueGetter = (description /*: { label, value }[] | string */) => {
+	// As an array: direct values not exported into enums module
+	if (Array.isArray(description)) {
+		return simpleFind(description)
+	}
+	// Name of simple enum
+	else if (typeof description === 'string' && description.indexOf(':') === -1) {
+		const values = getSimpleEnumValues(description)
+		if (values) {
+			return simpleFind(values)
 		} else {
-			// Nested enum
-			const [enumName, targetPath] = zenum.split(':')
-			if (nested[enumName]) {
-				return nestedFind(nested[enumName], targetPath)
-			} else {
-				throw Error(`Invalid nested enum "${enumName}" (from "${zenum}"), check your schema and/or enums.nested.json`)
-			}
+			throw Error(`Invalid enum "${description}", check your schema and/or enums.json`)
 		}
-	} else if (zenum) {
-		throw Error(`Invalid enum type "${typeof zenum}"`)
+	}
+	// Description of nested enum, format 'name:path'
+	else if (typeof description === 'string') {
+		const [name, path] = description.split(':')
+		const values = getNestedEnumValues(name)
+		if (values) {
+			return nestedFind(values, path)
+		} else {
+			throw Error(`Invalid nested enum "${name}" (from "${description}"), check your schema and/or enums.nested.json`)
+		}
+	}
+	// Unrecognized enum description
+	else if (description) {
+		throw Error(`Invalid enum type "${typeof description}"`)
 	}
 }
 
-const simpleFind = enums => value => enums && enums.find && enums.find(e => e && (e === value || e.value === value))
-
-const nestedFind = (enums, targetPath) => {
-	return (value, doc) => {
-		const targetValue = targetPath.split('/').reduce((d, p) => {
-			if (p === '.') {
-				return d
-			} else if (p === '..') {
-				return d.parent()
-			} else {
-				return d[p]
-			}
-		}, doc)
-		return simpleFind(enums[targetValue])(value)
+const simpleFind = enums => value => {
+	if (enums && enums.find) {
+		return enums.find(e => e && (e === value || e.value === value))
+	} else {
+		return null
 	}
 }
+
+const nestedFind = (enums, targetPath) => (value, doc) => {
+	const targetValue = targetPath.split('/').reduce((d, p) => {
+		if (p === '.') {
+			return d
+		} else if (p === '..') {
+			return d.parent()
+		} else {
+			return d[p]
+		}
+	}, doc)
+	return simpleFind(enums[targetValue])(value)
+}
+
+const getNestedEnumValues = exports.getNestedEnumValues = name => {
+	return nestedEnums[name] || null
+}
+
+const getSimpleEnumValues = exports.getSimpleEnumValues = memoize(name => {
+	// Special cases: some enums are just keys in enums.json and must be matched with another file
+
+	// nationalities
+	if (name === 'nationalities') {
+		return enums.nationalities.map(value => {
+			const { nationalityLabel: label } = countries.find(c => c.alpha2 === value)
+			return { value, label }
+		})
+	}
+
+	// iso4217 === currencies
+	if (name === 'currencies' || name === 'iso4217') {
+		return enums.iso4217.map(value => {
+			const { label, symbol } = currencies.find(c => c.code === value)
+			return { value, label, symbol } // TODO use symbol?
+		})
+	}
+
+	// iso6391 === languages
+	if (name === 'languages' || name === 'iso6391') {
+		return enums.iso6391.map(value => {
+			const { label } = languages.find(c => c['639-1'] === value)
+			return { value, label } // TODO use symbol?
+		})
+	}
+
+	// countries
+	if (name === 'countries') {
+		return enums.countries.map(value => {
+			const { countryLabel: label } = countries.find(c => c.alpha2 === value)
+			return { value, label }
+		})
+	}
+
+	// Other cases: no need to reformat
+	return enums[name] || null
+})
--- server/routes/enums.js
+++ server/routes/enums.js
@@ -2,72 +2,29 @@
 
 const { Router } = require('express')
 const { ClientError } = require('../lib/errors')
-const enums = require('../../specs/enums.json')
-//const specials = require('../../specs/enums.special.js')
-const nestedEnums = require('../../specs/enums.nested.json')
+const { getSimpleEnumValues, getNestedEnumValues } = require('../lib/enums')
 const { restHandler } = require('../lib/rest-utils')
-
-const countries = require('../../specs/enum.countries.json')
-const currencies = require('../../specs/enum.currencies.json')
-const languages = require('../../specs/enum.languages.json')
 
 
 module.exports = Router()
-//.get('/special/:name', restHandler(getSpecialEnum))
-.get('/:name', restHandler(getEnum))
-.get('/nested/:name', restHandler(getNestedEnum))
+.get('/:name', restHandler(simple))
+.get('/nested/:name', restHandler(nested))
 
-function getEnum (req) {
-	if (req.params.name in enums) {
-		return formatEnum(req.params.name, enums[req.params.name])
+
+function simple (req) {
+	const values = getSimpleEnumValues(req.params.name)
+	if (values) {
+		return values
 	}
 
 	throw new ClientError({ title: `Unknown enum "${req.params.name}"`, status: 404 })
 }
 
-function getNestedEnum (req) {
-	if (req.params.name in nestedEnums) {
-		return nestedEnums[req.params.name]
+function nested (req) {
+	const values = getNestedEnumValues(req.params.name)
+	if (values) {
+		return values
 	}
 
 	throw new ClientError({ title: `Unknown nested enum "${req.params.name}"`, status: 404 })
-}
-
-function formatEnum (name, data) {
-	// Special cases: some enums are just keys in enums.json and must be matched with another file
-
-	// nationalities
-	if (name === 'nationalities') {
-		return data.map(value => {
-			const { nationalityLabel: label } = countries.find(c => c.alpha2 === value)
-			return { value, label }
-		})
-	}
-
-	// iso4217 === currencies
-	if (name === 'currencies' || name === 'iso4217') {
-		return data.map(value => {
-			const { label, symbol } = currencies.find(c => c.code === value)
-			return { value, label, symbol } // TODO use symbol?
-		})
-	}
-
-	// iso6391 === languages
-	if (name === 'languages' || name === 'iso6391') {
-		return data.map(value => {
-			const { label } = languages.find(c => c['639-1'] === value)
-			return { value, label } // TODO use symbol?
-		})
-	}
-
-	// countries
-	if (name === 'countries') {
-		return data.map(value => {
-			const { countryLabel: label } = countries.find(c => c.alpha2 === value)
-			return { value, label }
-		})
-	}
-
-	// Other cases: no need to reformat
-	return data
 }
