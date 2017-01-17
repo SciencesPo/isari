5aba0adf404ddabf58d9066ee6a4e8e83a86a04c
Yomguithereal <guillaumeplique@gmail.com>
2017-01-17 15:06:03 +0100

    Refactoring annex4 generation

--- server/export/annex4.js
+++ server/export/annex4.js
@@ -2,31 +2,76 @@
  * ISARI HCERES Annex 4 Export Routine
  * ====================================
  */
-const Handlebars = require('handlebars');
+const Handlebars = require('handlebars'),
+      parseDate = require('./helpers.js').parseDate;
 
+/**
+ * Handlebars templates.
+ */
 const TEMPLATES = {
-  1: `
-    <h1>Participation à des comités éditoriaux (revues, collections)</h1>
-    <ul>
-      {{#each firstGroup}}
-        {{#each activities}}
-          <li>{{../firstName}} {{../name}}: {{description}}</li>
+  groups: `
+    {{#each groups}}
+      <h1>{{title}}</h1>
+      <ul>
+        {{#each people}}
+          {{#each activities}}
+            <li>{{../firstName}} {{../name}}, {{description}}{{#if startDate}}, {{formatRange .}}{{/if}}</li>
+          {{/each}}
         {{/each}}
-      {{/each}}
-    </ul>
-    <h1>Direction de collections et de séries</h1>
-    <ul>
-      {{#each secondGroup}}
-        {{#each activities}}
-          <li>{{../firstName}} {{../name}}: {{description}}</li>
-        {{/each}}
-      {{/each}}
-    </ul>
+      </uL>
+    {{/each}}
   `
 };
 
 for (const k in TEMPLATES)
   TEMPLATES[k] = Handlebars.compile(TEMPLATES[k]);
+
+/**
+ * Helpers.
+ */
+function formatRange(item) {
+  let {startDate, endDate} = item;
+
+  if (startDate)
+    startDate = parseDate(startDate);
+  if (endDate)
+    endDate = parseDate(endDate);
+
+  if (!startDate && !endDate)
+    return null;
+
+  if (startDate && !endDate)
+    return `depuis ${startDate.format('DD/MM/YYYY')}`;
+
+  if (!endDate)
+    return null;
+
+  return `${startDate.format('DD/MM/YYYY')} - ${endDate.format('DD/MM/YYYY')}`;
+}
+Handlebars.registerHelper('formatRange', formatRange);
+
+function computeGroups(definitions, people) {
+  return definitions.map(definition => {
+    const {title, predicate, mapper} = definition;
+
+    const groupPeople = people
+      .filter(person => {
+        return (
+          person.personalActivities &&
+          person.personalActivities.some(predicate)
+        );
+      })
+      .map(person => {
+        return {
+          firstName: person.firstName,
+          name: person.name,
+          activities: person.personalActivities.filter(predicate)
+        };
+      });
+
+    return {title, people: groupPeople};
+  });
+}
 
 /**
  * Process.
@@ -37,39 +82,38 @@ module.exports = {
   1(models, centerId, callback) {
     const People = models.People;
 
-    function firstGroupPredicate(personalActivity) {
-      return (
-        personalActivity.personalActivityType === 'editorial' &&
-        (
-          !personalActivity.personalActivitySubtype ||
-          personalActivity.personalActivitySubtype === 'revueScientifique' ||
-          (
-            personalActivity.personalActivitySubtype === 'collectionScientifique' &&
-            personalActivity.role === 'membre'
-          )
-        )
-      );
-    }
-
     const secondGroupRoles = new Set(['direction', 'codirection', 'présidence']);
 
-    function secondGroupPredicate(personalActivity) {
-      return (
-        personalActivity.personalActivityType === 'editorial' &&
-        (
-          personalActivity.personalActivitySubtype === 'collectionScientifique' &&
-          secondGroupRoles.has(personalActivity.role)
-        )
-      );
-    }
-
-    function groupMapper(predicate, person) {
-      return {
-        firstName: person.firstName,
-        name: person.name,
-        activities: person.personalActivities.filter(predicate)
-      };
-    }
+    const groupDefinitions = [
+      {
+        title: 'Participation à des comités éditoriaux (revues, collections)',
+        predicate(personalActivity) {
+          return (
+            personalActivity.personalActivityType === 'editorial' &&
+            (
+              !personalActivity.personalActivitySubtype ||
+              personalActivity.personalActivitySubtype === 'revueScientifique' ||
+              (
+                personalActivity.personalActivitySubtype === 'collectionScientifique' &&
+                personalActivity.role === 'membre'
+              )
+            )
+          );
+        }
+      },
+      {
+        title: 'Direction de collections et de séries',
+        predicate(personalActivity) {
+          return (
+            personalActivity.personalActivityType === 'editorial' &&
+            (
+              personalActivity.personalActivitySubtype === 'collectionScientifique' &&
+              secondGroupRoles.has(personalActivity.role)
+            )
+          );
+        }
+      }
+    ];
 
     // Finding people having an academicMembership on our center
     return People.find({
@@ -80,29 +124,29 @@ module.exports = {
 
       people = people.map(person => person.toObject());
 
-      const firstGroup = people
-        .filter(person => {
-          return (
-            person.personalActivities &&
-            person.personalActivities.some(firstGroupPredicate)
-          );
-        })
-        .map(groupMapper.bind(null, firstGroupPredicate));
-
-      const secondGroup = people
-        .filter(person => {
-          return (
-            person.personalActivities &&
-            person.personalActivities.some(secondGroupPredicate)
-          );
-        })
-        .map(groupMapper.bind(null, secondGroupPredicate));
+      const groups = computeGroups(groupDefinitions, people);
 
       // Applying template
-      const html = TEMPLATES[1]({
-        firstGroup,
-        secondGroup
-      });
+      const html = TEMPLATES.groups({groups});
+
+      return callback(null, html);
+    });
+  },
+
+  2(models, centerId, callback) {
+    const People = models.People;
+
+    // Finding people having an academicMembership on our center
+    return People.find({
+      'academicMemberships.organization': centerId
+    }, (err, people) => {
+      if (err)
+        return callback(err);
+
+      people = people.map(person => person.toObject());
+
+      // Applying template
+      const html = TEMPLATES[2]({});
 
       return callback(null, html);
     });
