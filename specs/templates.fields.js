c3554a25f0791dadb7e8a1a3790f26635084b1fb
Paul Girard <paul.girard@sciencespo.fr>
2017-01-27 16:38:33 +0100

    templates taking range from scope into account

--- specs/templates.fields.js
+++ specs/templates.fields.js
@@ -3,35 +3,36 @@
 //****** utils
 const { formatEnum } = require('../server/lib/enums')
 const moment = require('../server/node_modules/moment')
+const _ = require('../server/node_modules/lodash')
 const { overlap } = require('../server/export/helpers')
 
 // datesPeriod
 
 function prettyPrintTimePeriod(g){
-	if (g.startDate && g.endDate) {
-			return year(g.startDate) + '-' + year(g.endDate)
-	} else if (g.startDate) {
-		return  year(g.startDate) + '-…'
-	} else if (g.endDate) {
-		return ' …-' + year(g.endDate)
-	}
+  if (g.startDate && g.endDate) {
+      return year(g.startDate) + '-' + year(g.endDate)
+  } else if (g.startDate) {
+    return  year(g.startDate) + '-…'
+  } else if (g.endDate) {
+    return ' …-' + year(g.endDate)
+  }
 }
 
 
 // Date parsing
 const date = string => {
-	const [ year, month, day ] = string.split('-')
-	return { year, month, day }
+  const [ year, month, day ] = string.split('-')
+  return { year, month, day }
 }
 const year = string => date(string).year
 
 // People
 const peopleName = p => {
-	let label = p.name
-	if (p.firstName) {
-		label = p.firstName + ' ' + label
-	}
-	return label
+  let label = p.name
+  if (p.firstName) {
+    label = p.firstName + ' ' + label
+  }
+  return label
 }
 
 // other objects name
@@ -39,73 +40,79 @@ const objectName = o => o.name
 
 // oraganizationDates
 function organizationDates(ps){
-	const formatPosition = p => {
-		if (!p.organization) {
-		return ''
-		}
-		let label = p.organization.acronym ? p.organization.acronym : p.organization.name
-		label += prettyPrintTimePeriod(p)
-		return label
-	}
+  const formatPosition = p => {
+    if (!p.organization) {
+    return ''
+    }
+    let label = p.organization.acronym ? p.organization.acronym : p.organization.name
+    label += prettyPrintTimePeriod(p)
+    return label
+  }
 
-	if (Array.isArray(ps))
-		return ps.map(p => formatPosition(p)).join(";")
-	else
-		return formatPosition(p)	
+  if (Array.isArray(ps))
+    return ps.map(p => formatPosition(p)).join(";")
+  else
+    return formatPosition(p)  
 }
 
-function currentMemberships(afs,scope){
-	if (!afs)
-		return '';
-	const thisMonth = moment().format('YYYY-MM') 
-	// keep only memberships active this month
-	const r = afs.filter(a => a.membershipType !== 'visiting' && a.membershipType !== 'associé')
-				.filter(a => overlap(a,{'startDate': thisMonth,'endDate': thisMonth}))
-		// sort orga from scope first and then by alphabetic order 
-		.sort(e => (e.organization._id.toString() === scope.organization) ? 'aaaaaaaaaa' : e.organization.acronym || e.organization.name)
-	if (r.length){
-	  	// take the last one
-	  	return r.map(af => af.organization.acronym || af.organization.name).join(", ");
-	}
-	
-	return '';
+function memberships(afs,scope){
+  if (!afs)
+    return '';
+  const thisMonth = moment().format('YYYY-MM') 
+          // remove lab aff when visiting or associate
+  const r = _.sortBy(
+    afs.filter(a => a.membershipType !== 'visiting' && a.membershipType !== 'associé')
+        // keep only memberships active this month
+       .filter(a => overlap(a, {
+          'startDate': scope.start ? scope.start : thisMonth, 
+          'endDate': scope.end ? scope.end : thisMonth
+        }))
+    ,
+    // sort orga from scope first and then by alphabetic order
+    [a => a.organization._id.toString() === scope.organization ? 'AAAAAAAAA' : (a.organization.acronym || a.organization.name)] 
+  )
+  
+  if (r.length)
+      return r.map(af => af.organization.acronym || af.organization.name).join(", ");
+  
+  return '';
 
 }
 
 
 // department Memberships Dates
 function deptMembershipsDates(p){
-	if (!p.departement) {
-		return ''
-	}
-	return formatEnum('teachingDepartements',p.departement,label =>{
-		label += ' '+prettyPrintTimePeriod(p)
-		return label
-	}) 
+  if (!p.departement) {
+    return ''
+  }
+  return formatEnum('teachingDepartements',p.departement,label =>{
+    label += ' '+prettyPrintTimePeriod(p)
+    return label
+  }) 
 }
 
 function currentDeptMembershipsDates(p){
-	if (p){
-		const thisMonth = moment().format('YYYY-MM')
-		// keep only memberships active this month
-	  	const afs = p.filter(a => overlap(a,{'startDate': thisMonth,'endDate': thisMonth}))
-	  				// sort orga from scope first and then by alphabetic order 
-	  				.sort(e => e.departement)
-	  if (afs.length){
-	  	// take the last one
-	  	return afs.map(af => formatEnum('teachingDepartements', af.departement));
-	  }
-	}
-  	return '';
+  if (p){
+    const thisMonth = moment().format('YYYY-MM')
+    // keep only memberships active this month
+      const afs = p.filter(a => overlap(a,{'startDate': thisMonth,'endDate': thisMonth}))
+            // sort orga from scope first and then by alphabetic order 
+            .sort(e => e.departement)
+    if (afs.length){
+      // take the last one
+      return afs.map(af => formatEnum('teachingDepartements', af.departement));
+    }
+  }
+    return '';
   }
 
 
 // personnalActivities
 function personalActivity(p) {
-	if (!Array.isArray(p))
-		p = [p]
+  if (!Array.isArray(p))
+    p = [p]
 
-	return p.map(e => formatEnum('personalActivityTypes', e.personalActivityType, label => label + ' ' + prettyPrintTimePeriod(p))).join(';')
+  return p.map(e => formatEnum('personalActivityTypes', e.personalActivityType, label => label + ' ' + prettyPrintTimePeriod(p))).join(';')
 }
 
 // distinctions
@@ -113,41 +120,41 @@ const distinction = d => d.map(e => e.title).join(';')
 
 // oraganizationDates
 function researchUnitCode(p){
-	let label = p.code
-	if(p.startDate || p.enDate)
-		label+=" ("
-	if(p.startDate){
-		label += year(p.startDate)
-	}
-	if(p.endDate){
-		label += `-${year(p.endDate)}`
-	}
-	else{
-		if(p.starDate)
-			label += "..."
-	}
-	if(p.startDate || p.enDate)
-		label+=")"
-	return label
+  let label = p.code
+  if(p.startDate || p.enDate)
+    label+=" ("
+  if(p.startDate){
+    label += year(p.startDate)
+  }
+  if(p.endDate){
+    label += `-${year(p.endDate)}`
+  }
+  else{
+    if(p.starDate)
+      label += "..."
+  }
+  if(p.startDate || p.enDate)
+    label+=")"
+  return label
 }
 
 // peopleDates
 function peopleDates(p){
-	let label = p.people ? peopleName(p.people) : ''
-	if(p.startDate){
-		label += " "+year(p.startDate)
-	}
-	if(p.endDate){
-		label+=`-${year(p.endDate)}`
-	}
-	return label
+  let label = p.people ? peopleName(p.people) : ''
+  if(p.startDate){
+    label += " "+year(p.startDate)
+  }
+  if(p.endDate){
+    label+=`-${year(p.endDate)}`
+  }
+  return label
 }
 
 // grades
 function peopleGrades(g){
-	if (!Array.isArray(g))
-		g = [g]
-	return g.map(e => formatEnum('gradeStatus', e.gradeStatus,label => {return label + ' '+ prettyPrintTimePeriod(e)})).join(";") 
+  if (!Array.isArray(g))
+    g = [g]
+  return g.map(e => formatEnum('gradeStatus', e.gradeStatus,label => {return label + ' '+ prettyPrintTimePeriod(e)})).join(";") 
 }
 
 exports.peopleName = peopleName
@@ -160,4 +167,4 @@ exports.researchUnitCode = researchUnitCode
 exports.peopleDates = peopleDates
 exports.peopleGrades = peopleGrades
 exports.organizationDates = organizationDates
-exports.currentMemberships = currentMemberships
+exports.memberships = memberships
