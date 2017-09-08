3e68f44379a577d43c73ac58cbc63961934db026
Nicolas Chambrier <naholyr@gmail.com>
2017-09-08 14:57:36 +0200

    feat(edit-logs): NON WORKING query to filter edits with ONLY confidential changes

--- server/routes/editLog.js
+++ server/routes/editLog.js
@@ -191,6 +191,57 @@ function getEditLog(req, res){
 					return res.status(200).send([])
 			}
 
+			// Filter diffs which contain ONLY confidential changes
+			// Note: we don't check for create/delete, assuming there can't be creation involving ONLY confidential fields
+			if (!canViewConfidential && (!query.action || query.action === 'update')) {
+				/*
+				The main difficulty here is dealing with paths as arrays. Let's write it all down to make it simpler.
+				confidential paths = [ 'x.*.y', 'm.n' ]
+				We want to EXLUDE all edits whose diff contains ONLY changes on confidential paths
+				(thank you mongodb for the headache)
+
+				I want docs whose ALL subdocs satisfy CONDITION
+				.find({ subdocs: { $elemMatch: CONDITION } }) → all docs whose SOME subdocs satisfy CONDITION
+				.find({ subdocs: { $elemMatch: !CONDITION } }) → all docs whose SOME subdocs DO NOT satisfy CONDITION
+				.find({ subdocs: { $not: {$elemMatch:!CONDITION}}}) → all docs whose NO subdocs DO NOT satisfy CONDITION
+
+				Initial condition to find a confidential change is:
+				(diff.path.0 = x && diff.path.2 = y) || (diff.path.0 = m && diff.path.1 = n)
+
+				We must negate it:
+				(diff.path.0 != x || diff.path.2 != y) && (diff.path.0 != m || diff.path.1 != n)
+
+				Final query:
+				{ diff: { $not: { $elemMatch: { $and: [
+					{ $or: [ {'path.0': {$ne: 'x'}}, {'path.2': {$ne: 'y'}} ] },
+					{ $or: [ {'path.0': {$ne: 'm'}}, {'path.1': {$ne: 'n'}} ] }
+				]}}}}
+
+				Note that there can already be a query on 'diff', just merge it.
+				{ diff: { oldquery, $not: newquery }}
+				If there is already a query on diff.$not, we'll have to merge it smartly:
+				!x && !y → !(x || y), so it becomes:
+				{ diff: { $not: { $or: [oldquery, newquery] }}}
+				*/
+				// [ 'p1.*.p2', 'p3.p4' ] → [ {$or:[path.0≠p1, path.2≠p2]}, {$or:[path.0≠p3, path.1≠p4]} ]
+				const conditions = computeConfidentialPaths(model)
+					.map(path =>
+						path.split('.')
+						.map((p, i) => p === '*' ? null : { ['path.' + i]: { '$ne': p } })
+						.filter(cond => cond !== null))
+					.map(q => ({ '$or': q }))
+				const negated = { '$elemMatch': { '$and': conditions } }
+				if (!mongoQuery['diff']) {
+					mongoQuery['diff'] = { '$not': negated }
+				} else if (!mongoQuery['diff']['$not']) {
+					mongoQuery['diff']['$not'] = negated
+				} else {
+					// Note this case does not exist today, but I wouldn't bet it's forever so be future-proof
+					const oldQuery = mongoQuery['diff']['$not']
+					mongoQuery['diff']['$not'] = { '$or': [ negated, oldQuery ] }
+				}
+			}
+
 			if (query.action)
 				mongoQuery['action'] = query.action
 
