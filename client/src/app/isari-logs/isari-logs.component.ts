83b500bf364fbd82e0725c9c9c843ba1fcf7c8be
t8g <tmoyse@gmail.com>
2017-10-30 09:45:09 +0100

    clean

--- client/src/app/isari-logs/isari-logs.component.ts
+++ client/src/app/isari-logs/isari-logs.component.ts
@@ -1,30 +1,31 @@
-import { TranslateService, LangChangeEvent } from 'ng2-translate';
-import { IsariDataService } from './../isari-data.service';
-import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from 'ng2-translate';
 
-import keyBy from 'lodash/keyBy';
-import flattenDeep from 'lodash/flattenDeep';
-import uniq from 'lodash/uniq';
 import { BehaviorSubject } from 'rxjs';
 import { EditLogApiOptions } from './EditLogApiOptions.class';
-import { ToasterService } from "angular2-toaster";
+import { IsariDataService } from './../isari-data.service';
+import { Observable } from 'rxjs/Observable';
+import { ToasterService } from 'angular2-toaster';
+import flattenDeep from 'lodash/flattenDeep';
+import keyBy from 'lodash/keyBy';
+import uniq from 'lodash/uniq';
 
 @Component({
   selector: 'isari-logs',
-  templateUrl: './isari-logs.component.html',
+  templateUrl: './isari-logs.component.html'
   // styleUrls: ['./isari-layout.component.css']
 })
 export class IsariLogsComponent implements OnInit {
-
   feature: string;
   options: EditLogApiOptions = { skip: 0, limit: 5 };
   options$: BehaviorSubject<EditLogApiOptions>;
   details$: BehaviorSubject<boolean>;
-  logs$: Observable<{ count: number, logs: Array<any> }>;
+  logs$: Observable<{ count: number; logs: Array<any> }>;
   labs$: Observable<any[]>;
 
   constructor(
@@ -33,55 +34,74 @@ export class IsariLogsComponent implements OnInit {
     private translate: TranslateService,
     private router: Router,
     private toasterService: ToasterService
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.options$ = new BehaviorSubject(this.options);
     this.details$ = new BehaviorSubject(false);
-    this.logs$ = Observable
-    .combineLatest([
+    this.logs$ = Observable.combineLatest([
       this.route.paramMap,
       this.options$,
       this.translate.onLangChange
         .map((event: LangChangeEvent) => event.lang)
         .startWith(this.translate.currentLang)
     ])
-    .switchMap(([paramMap, options, lang]) => {
-      this.feature = (<ParamMap>paramMap).get('feature');
-      this.options = Object.assign({}, {
-        itemID: (<ParamMap>paramMap).get('itemID')
-      }, options);
+      .switchMap(([paramMap, options, lang]) => {
+        this.feature = (<ParamMap>paramMap).get('feature');
+        this.options = Object.assign(
+          {},
+          {
+            itemID: (<ParamMap>paramMap).get('itemID')
+          },
+          options
+        );
 
-      return this.isariDataService
-      .getHistory(this.feature, this.options, lang)
-      .catch(err => {
-        if (err.status === 401) {
-          this.translate.get('error.' + err.status).subscribe(translated => {
-            this.toasterService.pop('error', '', translated);
-            this.router.navigate(['/', this.feature], { preserveQueryParams: true });
-          });
-        }
-        return Observable.throw(err);
+        return this.isariDataService
+          .getHistory(this.feature, this.options, lang)
+          .catch(err => {
+            if (err.status === 401) {
+              this.translate
+                .get('error.' + err.status)
+                .subscribe(translated => {
+                  this.toasterService.pop('error', '', translated);
+                  this.router.navigate(['/', this.feature], {
+                    preserveQueryParams: true
+                  });
+                });
+            }
+            return Observable.throw(err);
+          })
+          .combineLatest(this.details$);
       })
-      .combineLatest(this.details$)
-    })
-    .map(([{count, logs}, details]) => {
-      this.labs$ = this.isariDataService.getForeignLabel('Organization', uniq(flattenDeep(logs.map(log => log.who.roles.map(role => role.lab)))))
-        .map(labs => keyBy(labs, 'id'));
+      .map(([{ count, logs }, details]) => {
+        this.labs$ = this.isariDataService
+          .getForeignLabel(
+            'Organization',
+            uniq(
+              flattenDeep(logs.map(log => log.who.roles.map(role => role.lab)))
+            )
+          )
+          .map(labs => keyBy(labs, 'id'));
 
-      if (details && this.options['path']) return ({
-        count,
-        logs: logs.map(log => Object.assign({}, log, {
-          _open: true,
-          diff: log.diff.filter(diff => diff.path[0] === this.options['path'])
-        }))
+        if (details && this.options['path']) {
+          return {
+            count,
+            logs: logs.map(log =>
+              Object.assign({}, log, {
+                _open: true,
+                diff: log.diff.filter(
+                  diff => diff.path[0] === this.options['path']
+                )
+              })
+            )
+          };
+        }
+
+        return {
+          count,
+          logs: logs.map(log => Object.assign({}, log, { _open: details }))
+        };
       });
-
-      return {
-        count,
-        logs: logs.map(log => Object.assign({}, log, { _open: details }))
-      };
-    });
   }
 
   changeOpt(options) {
@@ -93,8 +113,14 @@ export class IsariLogsComponent implements OnInit {
     this.details$.next(!this.details$.value);
   }
 
-  exportLogs({logs, filetype}) {
-    this.isariDataService.exportLogs(logs, this.feature, this.labs$, this.translate, this.details$.value, filetype);
+  exportLogs({ logs, filetype }) {
+    this.isariDataService.exportLogs(
+      logs,
+      this.feature,
+      this.labs$,
+      this.translate,
+      this.details$.value,
+      filetype
+    );
   }
-
 }
